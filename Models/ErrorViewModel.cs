4cfa4988ff77a3e1e7788370380612e190654706
tanric-ICS <69907691+tanric-ICS@users.noreply.github.com>
2022-10-08 16:07:54 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,1 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace TicketManager.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
 
